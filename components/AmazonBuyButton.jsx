9ea4a255503aec60823873967d73d524ee96ea25
Swagat <swagat@xquantum.in>
2026-08-03 19:03:40 +0530

    DirectBuy

--- components/AmazonBuyButton.jsx
+++ components/AmazonBuyButton.jsx
@@ -4,9 +4,12 @@ import Icon from './Icon';
 export default function AmazonBuyButton({ affiliateLink, title, className = '' }) {
   if (!affiliateLink) return null;
 
-  const params = new URLSearchParams({ to: affiliateLink });
-  if (title) params.set('title', title);
-  const href = `/buy?${params.toString()}`;
+  // Linking straight to Amazon for now, skipping the /buy interstitial.
+  // To bring it back, swap href for the /buy?to=...&title=... version below.
+  const href = affiliateLink;
+  // const params = new URLSearchParams({ to: affiliateLink });
+  // if (title) params.set('title', title);
+  // const href = `/buy?${params.toString()}`;
 
   return (
     <div className={`amazon-buy ${className}`} onClick={(e) => e.stopPropagation()}>
